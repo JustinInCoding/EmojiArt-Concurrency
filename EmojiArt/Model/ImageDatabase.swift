96f0621623da22a9377a59f1600d0634511360e7
JustinSheng <justin.wang4@icloud.com>
2024-04-05 15:32:26 +0800

    Creating a GlobalActor

--- EmojiArt/Model/ImageDatabase.swift
+++ EmojiArt/Model/ImageDatabase.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2022 Razeware LLC
+/// Copyright (c) 2024 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -30,54 +30,60 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import UIKit
 
-@ImageDatabase class DiskStorage {
-  private var folder: URL
-
-  init() {
-    guard let supportFolderURL = FileManager.default
-      .urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
-      fatalError("Couldn't open the application support folder")
-    }
-    let databaseFolderURL = supportFolderURL.appendingPathComponent("database")
-
-    do {
-      try FileManager.default.createDirectory(at: databaseFolderURL, withIntermediateDirectories: true, attributes: nil)
-    } catch {
-      fatalError("Couldn't create the application support folder")
-    }
-
-    folder = databaseFolderURL
-  }
-
-  nonisolated static func fileName(for path: String) -> String {
-    return path.dropFirst()
-      .components(separatedBy: .punctuationCharacters)
-      .joined(separator: "_")
-  }
-
-  func write(_ data: Data, name: String) throws {
-    try data.write(to: folder.appendingPathComponent(name), options: .atomic)
-  }
-
-  func read(name: String) throws -> Data {
-    return try Data(contentsOf: folder.appendingPathComponent(name))
-  }
-
-  func remove(name: String) throws {
-    try FileManager.default.removeItem(at: folder.appendingPathComponent(name))
-  }
-
-  func persistedFiles() throws -> [URL] {
-    var result: [URL] = []
-    guard let directoryEnumerator = FileManager.default
-      .enumerator(at: folder, includingPropertiesForKeys: []) else {
-        throw "Could not open the application support folder"
-      }
-    for case let fileURL as URL in directoryEnumerator {
-      result.append(fileURL)
-    }
-    return result
-  }
+@globalActor actor ImageDatabase {
+	static let shared = ImageDatabase()
+	let imageLoader = ImageLoader()
+	private var storage: DiskStorage!
+	private var storedImageIndex = Set<String>()
+	
+	func setUp() async throws {
+		storage = await DiskStorage()
+		for fileURL in try await storage.persistedFiles() {
+			storedImageIndex.insert(fileURL.lastPathComponent)
+		}
+	}
+	
+	func store(image: UIImage, for key: String) async throws {
+		guard let data = image.pngData() else {
+			throw "Could not save image \(key)"
+		}
+		let fileName = DiskStorage.fileName(for: key)
+		try await storage.write(data, name: fileName)
+		storedImageIndex.insert(fileName)
+	}
+	
+	func image(_ key: String) async throws -> UIImage {
+		let keys = await imageLoader.cache.keys
+		if keys.contains(key) {
+			print("In memory cache.")
+			return try await imageLoader.image(key)
+		}
+		do {
+			let fileName = DiskStorage.fileName(for: key)
+			if !storedImageIndex.contains(fileName) {
+				throw "Image not persisted."
+			}
+			let data = try await storage.read(name: fileName)
+			guard let image = UIImage(data: data) else {
+				throw "Invalide image data."
+			}
+			print("In disk cache.")
+			await imageLoader.add(image, forKey: key)
+			return image
+		} catch {
+			let image = try await imageLoader.image(key)
+			try await store(image: image, for: key)
+			return image
+		}
+	}
+	
+	func clear() async {
+		for name in storedImageIndex {
+			try? await storage.remove(name: name)
+		}
+		storedImageIndex.removeAll()
+	}
+	
 }
